0ea4a7d4017e87c993a14cd32c5a4983072c27a2
PrestonMott <prestonmott@gmail.com>
2017-05-12 10:37:26 -0700

    Added name bar and button to UI.

--- MyContactApp/app/src/main/java/com/example/mottp7116/mycontactapp/DatabaseHelper.java
+++ MyContactApp/app/src/main/java/com/example/mottp7116/mycontactapp/DatabaseHelper.java
@@ -10,24 +10,29 @@ import android.util.StringBuilderPrinter;
  */
 public class DatabaseHelper extends SQLiteOpenHelper {
 
-    public static final String DATABASE_NAME = "Contact#";
+    public static final String DATABASE_NAME = "Contact.db";
     public static final String TABLE_NAME = "Contact_Table";
     public static final String COL_1 = "ID";
     public static final String COL_2 = "Name";
 
 
 
-    public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, DATABASE_NAME, factory, version);
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null,1);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
 
+        db.execSQL("CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT)");
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
 
     }
 }
