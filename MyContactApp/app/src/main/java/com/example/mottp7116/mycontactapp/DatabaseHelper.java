beab887f7e7277b9ccc8bf34d29062f1f76e1fb0
PrestonMott <prestonmott@gmail.com>
2017-05-23 09:44:38 -0700

    Search added

--- MyContactApp/app/src/main/java/com/example/mottp7116/mycontactapp/DatabaseHelper.java
+++ MyContactApp/app/src/main/java/com/example/mottp7116/mycontactapp/DatabaseHelper.java
@@ -1,28 +1,28 @@
 package com.example.mottp7116.mycontactapp;
 
+import android.app.SearchManager;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.util.StringBuilderPrinter;
 
-/**
- * Created by mottp7116 on 5/11/2017.
- */
+
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "Contact.db";
-    public static final String TABLE_NAME = "Contact_Table";
+    public static final String TABLE_NAME ="contact_table";
     public static final String COL_1 = "ID";
-    public static final String COL_2 = "Name";
-    public static final String COL_3 = "Age";
-    public static final String COL_4 = "Address";
+    public static final String COL_2 = "NAME";
+    public static final String COL_3 = "AGE";
+    public static final String COL_4 = "ADDRESS";
+
 
 
 
     public DatabaseHelper(Context context) {
-        super(context, DATABASE_NAME, null,2);
+        super(context, DATABASE_NAME, null, 2);
     }
 
     @Override
@@ -39,8 +39,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         onCreate(db);
 
     }
-
-    public boolean insertData(String name, String age, String address) {
+    public boolean insertData(String name, String age, String address){
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
         contentValues.put(COL_2, name);
@@ -52,10 +51,11 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         else return true;
     }
 
-
     public Cursor getAllData(){
+
         SQLiteDatabase db = this.getWritableDatabase();
-        Cursor res = db.rawQuery("select * from " + TABLE_NAME,null);
+        Cursor res = db.rawQuery("select * from " + TABLE_NAME, null);
         return res;
     }
+
 }
--- MyContactApp/app/src/main/java/com/example/mottp7116/mycontactapp/MainActivity.java
+++ MyContactApp/app/src/main/java/com/example/mottp7116/mycontactapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.mottp7116.mycontactapp;
 
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteQueryBuilder;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,7 +19,10 @@ public class MainActivity extends AppCompatActivity {
     EditText editName;
     EditText editAge;
     EditText editAddress;
-    Button btnAddData;
+    EditText editSearch;
+    Button btnaddData;
+    Button btnviewData;
+
 
 
     @Override
@@ -26,58 +32,108 @@ public class MainActivity extends AppCompatActivity {
 
         myDb = new DatabaseHelper(this);
 
+        //add the layout bars
         editName = (EditText) findViewById(R.id.editText_Name);
         editAge = (EditText) findViewById(R.id.editText_Age);
         editAddress = (EditText) findViewById(R.id.editText_Address);
+        editSearch = (EditText) findViewById(R.id.editText_search);
     }
 
+
     public void addData(View v) {
+
         boolean isInserted = myDb.insertData(editName.getText().toString(),editAge.getText().toString(),editAddress.getText().toString());
         Context context = getApplicationContext();
         CharSequence text1 = "Data Added!";
-        CharSequence text2 = "Failed to Insert Data!";
+        CharSequence text2 = "Failed!";
         int duration = Toast.LENGTH_SHORT;
 
         if (isInserted == true) {
             Log.d("MyContact", "Success inserting data");
+            //insert Toast message here
             Toast toast = Toast.makeText(context,text1,duration);
             toast.show();
-        }
-        else {
+
+
+        } else {
             Log.d("MyContact", "Failure inserting data");
+            //insert Toast message here
             Toast toast = Toast.makeText(context,text2,duration);
             toast.show();
+
         }
     }
 
     public void viewData(View v) {
+
+        Context context = getApplicationContext();
         Cursor res = myDb.getAllData();
         if (res.getCount() == 0) {
-            showMessage("Error", "No data is found in the data base");
-            //output message using Log.d and toast
+            showMessage("Error", "No data is found in the database");
+            //output a message using Log.d and toast
             return;
         }
+
         StringBuffer buffer = new StringBuffer();
-        for(int i = 0; i < res.getColumnCount(); i++) {
+        //setup a loop with the Cursor (res) using moveToNext
+        for (int i = 0; i < res.getColumnCount(); i++) {
             buffer.append(res.getColumnName(i));
             buffer.append("\t");
         }
         buffer.append("\n");
         res.moveToFirst();
-        do{
-            for(int i= 0; i < res.getColumnCount(); i++) {
+        do {
+
+            for (int i = 0; i < res.getColumnCount(); i++) {
                 buffer.append(res.getString(i));
                 buffer.append("\t");
             }
+
             buffer.append("\n");
         }
-        while(res.moveToNext());
+        while (res.moveToNext()) ;
         showMessage("Data", buffer.toString());
     }
 
-    private void showMessage(String title, String message) {
-        //AlertDialog.Builder
+
+    public void Search(View v)
+    {
+
+        Cursor c = myDb.getAllData();
+        StringBuffer b = new StringBuffer();
+
+        if (c != null)
+        {
+            c.moveToFirst();
+
+            for (int i = 0; i < c.getCount(); i++)
+            {
+                if (c.getString(1).equals(editSearch.getText().toString()))
+                {
+                    for (int j = 0; j < c.getColumnNames().length; j++)
+                    {
+                        b.append(c.getString(j) + "\n");
+                    }
+                    b.append("\n");
+                }
+                c.moveToNext();
+            }
+            showMessage("Contact Name: " + editSearch.getText().toString(), b.toString());
+        }
     }
 
+
+
+
+
+
+    private void showMessage(String title, String message) {
+        //AlertDialog.Builder
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.show();
+    }
 
 }
